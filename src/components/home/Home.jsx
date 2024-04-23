edd405490b0119d15a473f2f8ad971d5f7d205b8
rahulio96 <rahulgithub96@gmail.com>
2024-04-23 14:31:30 -0700

    Filter buttons work and change colors when clicked

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,13 +7,28 @@ function Home() {
 
     const [data, setData] = useState()
 
+    const [newBtnColor, setNewBtnColor] = useState(homeCSS.clicked)
+    const [topBtnColor, setTopBtnColor] = useState()
+
+    const sortByTop = () => {
+        setData([...data].sort((a, b) => b.upvotes - a.upvotes))
+        setTopBtnColor(homeCSS.clicked)
+        setNewBtnColor(homeCSS)
+    }
+
+    const sortByNew = () => {
+        setData([...data].sort((a, b) => Date.parse(b.created_at) - Date.parse(a.created_at)))
+        setNewBtnColor(homeCSS.clicked)
+        setTopBtnColor()
+    }
+
     useEffect(() => {
         const fetchPosts = async() => {
             const { data } = await supabase
                 .from('posts')
                 .select()
-            //console.log(data[0]title)
-            setData(data)
+            const sortedNew = data.sort((a, b) => Date.parse(b.created_at) - Date.parse(a.created_at))
+            setData(sortedNew)
         }
         fetchPosts()
     }, [])
@@ -23,8 +38,8 @@ function Home() {
         <div className={homeCSS.container}>
             <div className={homeCSS.filters}>
                 <h3>Filters:</h3>
-                <button>New</button>
-                <button>Top</button>
+                <button className={newBtnColor} onClick={sortByNew}>New</button>
+                <button className={topBtnColor} onClick={sortByTop}>Top</button>
             </div>
             <div className={homeCSS.cards}>
                 {data && Object.entries(data).map(([post]) => (
