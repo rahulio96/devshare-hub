c1315cad72e8ca9afc8c0bc17ebb883d71eed358
rahulio96 <rahgupta96@gmail.com>
2024-04-22 19:31:21 -0700

    Basic home page layout

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,28 @@
+import PostCard from "../post-card/PostCard"
+import homeCSS from "./Home.module.css"
+
 function Home() {
     return (
-        <>
-            
-        </>
+        <div className={homeCSS.container}>
+            <div className={homeCSS.filters}>
+                <h3>Filters:</h3>
+                <button>New</button>
+                <button>Top</button>
+            </div>
+            <div className={homeCSS.cards}>
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+            </div>
+        </div>
     )
 }
 
--- src/components/post-card/PostCard.jsx
+++ src/components/post-card/PostCard.jsx
@@ -1,8 +1,12 @@
+import postCardCSS from "./PostCard.module.css"
+
 function PostCard() {
     return (
-        <>
-            
-        </>
+        <div className={postCardCSS.container}>
+            <p>Posted 21 hours ago</p>
+            <h3>Check out my new project!</h3>
+            <p>11 upvotes</p>
+        </div>
     )
 }
 
