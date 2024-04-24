53f63fa341039fa25183d25b7e5e0ed786b0928e
rahulio96 <rahgupta96@gmail.com>
2024-04-24 10:20:09 -0700

    Added search button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import navbarCSS from './Navbar.module.css'
+import search from '/search.svg?url'
 
 function Navbar() {
     return (
       <nav>
         <div className={navbarCSS.title}>
           <h2>DevShare Hub</h2>
+          <div className={navbarCSS.searchSection}>
           <input type='text' placeholder='Search...'></input>
+          <button className={navbarCSS.searchBtn}><img className={navbarCSS.search} src={search} /></button>
+          </div>
         </div>
 
         <ul>
