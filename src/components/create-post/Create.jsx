51d9e63baea4a8afdbeadbe6fa1cef6962a75788
rahulio96 <rahgupta96@gmail.com>
2024-04-24 17:40:01 -0700

    Create page frontend

--- src/components/create-post/Create.jsx
+++ src/components/create-post/Create.jsx
@@ -1,8 +1,24 @@
+import createCSS from './Create.module.css'
+
 function Create() {
     return (
-        <>
-            <p>HIIII</p>
-        </>
+
+        <div className={createCSS.container}>
+            <h1>Create Post</h1>
+
+            <div className={createCSS.mainText}>
+                <input type='text' placeholder='Title*'></input>
+                <textarea placeholder='Body*'></textarea>
+                <h2>Add Tags</h2>
+                <input type='text' placeholder='ex: React, Figma, AWS'></input>
+                <h2>Add Project Link</h2>
+                <input type='text' placeholder='GitHub or Deployed Link'></input>
+            </div>
+            <div className={createCSS.btnContainer}>
+                <button className={createCSS.post}>Post</button>
+                <button className={createCSS.clear}>Clear</button> 
+            </div>
+        </div>
     )
 }
 
